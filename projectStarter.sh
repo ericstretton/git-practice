ab791182f885eaec5b7da3f03b198616824c5cc8
ericstretton <eric.stretton@gmail.com>
2022-01-27 20:58:45 -0600

    Setting up my Project

--- projectStarter.sh
+++ projectStarter.sh
@@ -2,7 +2,6 @@
 echo 'file name' $1;
 echo 'name of script' $0;
 echo '$0' $!;
-<I have no idea how to solve this lol> 
 
 mkdir 'basicBash';
 cd basicBash;
@@ -18,4 +17,3 @@ cd js;
 touch 'app.js';
 cd ..;
 printf 'All done!';
-<also really couldnt work out how to indent the All done part> 
